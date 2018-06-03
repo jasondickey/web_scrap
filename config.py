c6f95ddcb468fe8b5d2f8b65a976f64ed3305654
Jason Dickey <jd.glenn@ymail.com>
2018-06-03 15:50:49 -0700

    uploading hw

---
+++ config.py
@@ -0,0 +1,5 @@
+# Twitter API Keys
+consumer_key = "VUo5cQ0KP5UebABTIZsXgxX5h"
+consumer_secret = "i12ecV0JRun8QXJfKnkyx4TC38XKLHNHHdraVNnlWrlkqCZrHG"
+access_token = "980129452108627968-quXOK4PMetXR7oRA8bJfRcEGZJKeeJi"
+access_token_secret = "dC8Xd6YCMNw6BkZfTUXU5ls8t5yMnfLFPSJP5GjRi8Y92"
